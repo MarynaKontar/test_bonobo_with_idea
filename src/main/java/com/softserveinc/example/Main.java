14108bc735f68fab42b8a92c041e865d9c856000
mkont <mkont@softserveinc.com>
2020-05-14 14:11:52 +0300

    commit change master2

--- src/main/java/com/softserveinc/example/Main.java
+++ src/main/java/com/softserveinc/example/Main.java
@@ -3,10 +3,9 @@ package com.softserveinc.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello bonobo!");
-        System.out.println("commit change master");
+        System.out.println("commit change master1");
+        System.out.println("commit change master2");
         System.out.println("commit 2");
         System.out.println("commit with adding changes and rewrite commit");
-        System.out.println("test branch commit one");
-        System.out.println("test branch commit two");
     }
 }

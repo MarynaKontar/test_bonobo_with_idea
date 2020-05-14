59a016d66b62663ae24335b1dc98af8d4f9a7bb8
mkont <mkont@softserveinc.com>
2020-05-14 12:09:17 +0300

    test branch common commit

--- src/main/java/com/softserveinc/example/Main.java
+++ src/main/java/com/softserveinc/example/Main.java
@@ -3,5 +3,11 @@ package com.softserveinc.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello bonobo!");
+        System.out.println("commit change master1");
+        System.out.println("commit change master2");
+        System.out.println("commit 2");
+        System.out.println("commit with adding changes and rewrite commit");
+        System.out.println("test branch commit 1");
+        System.out.println("test branch commit 2");
     }
 }

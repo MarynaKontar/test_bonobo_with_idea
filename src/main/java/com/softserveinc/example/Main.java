d67bc6faef4dd03ad7ac9ed3b03791f0a2682ee5
mkont <mkont@softserveinc.com>
2020-05-14 12:09:17 +0300

    first  -  second commit with adding changes

--- src/main/java/com/softserveinc/example/Main.java
+++ src/main/java/com/softserveinc/example/Main.java
@@ -3,5 +3,8 @@ package com.softserveinc.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello bonobo!");
+        System.out.println("commit 1");
+        System.out.println("commit 2");
+        System.out.println("commit with adding changes and rewrite commit");
     }
 }
